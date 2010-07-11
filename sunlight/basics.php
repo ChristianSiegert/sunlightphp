2417873c76f8e07eb6637b3fe5a18f19eef7d77c
Christian Siegert <christian@christian-siegert.com>
2010-07-11 10:12:29 +0200

    HTML encoded output of debug().

--- sunlight/basics.php
+++ sunlight/basics.php
@@ -6,8 +6,11 @@
  */
 function debug($value) {
 	$backtrace = debug_backtrace();
-	$file = substr($backtrace[0]["file"], strlen(ROOT_DIR) + 1);
 
-	printf('<pre><span style="display: block; font-weight: bold;">%s (line %s)</span>%s</pre>', $file, $backtrace[0]["line"], print_r($value, true));
+	printf('<pre><span style="display: block; font-weight: bold;">%s (line %s)</span>%s</pre>',
+		substr($backtrace[0]["file"], strlen(ROOT_DIR) + 1),					// Filename
+		$backtrace[0]["line"],													// Line number
+		htmlentities(print_r($value, true), ENT_QUOTES, mb_internal_encoding())	// Text
+	);
 }
 ?>
