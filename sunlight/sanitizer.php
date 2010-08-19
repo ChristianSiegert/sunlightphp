dfa17eaa637656e00c21699c1aad61a30abf907f
Christian Siegert <christian@christian-siegert.com>
2010-08-19 03:29:10 +0200

    Added Sanitizer::normalize().

--- sunlight/sanitizer.php
+++ sunlight/sanitizer.php
@@ -7,5 +7,49 @@ class Sanitizer {
 	public static function decodeHtml($string) {
 		return html_entity_decode($string, ENT_QUOTES, mb_internal_encoding());
 	}
+
+	public static function normalize($string) {
+		// Map characters with diacritics on their base-character followed by
+		// the diacritical mark, i.e.  Ú => U´ and á => a`.
+		$string = Normalizer::normalize($string, Normalizer::FORM_D);
+
+		// Remove diacritics
+		$string = preg_replace('/\pM/u', "", $string);
+
+		// Map non-Latin characters to similar looking Latin ones
+		$string = preg_replace('/\x{00df}/u', "s", $string); // ß => s
+		$string = preg_replace('/\x{00c6}/u', "A", $string); // Æ => A
+		$string = preg_replace('/\x{00e6}/u', "a", $string); // æ => a
+		$string = preg_replace('/\x{0132}/u', "I", $string); // ? => I
+		$string = preg_replace('/\x{0133}/u', "i", $string); // ? => i
+		$string = preg_replace('/\x{0152}/u', "O", $string); // Œ => O
+		$string = preg_replace('/\x{0153}/u', "o", $string); // œ => o
+
+		$string = preg_replace('/\x{00d0}/u', "D", $string); // Ð => D
+		$string = preg_replace('/\x{0110}/u', "D", $string); // Ð => D
+		$string = preg_replace('/\x{00f0}/u', "d", $string); // ð => d
+		$string = preg_replace('/\x{0111}/u', "d", $string); // d => d
+		$string = preg_replace('/\x{0126}/u', "H", $string); // H => H
+		$string = preg_replace('/\x{0127}/u', "h", $string); // h => h
+		$string = preg_replace('/\x{0131}/u', "i", $string); // i => i
+		$string = preg_replace('/\x{0138}/u', "k", $string); // ? => k
+		$string = preg_replace('/\x{013f}/u', "L", $string); // ? => L
+		$string = preg_replace('/\x{0141}/u', "L", $string); // L => L
+		$string = preg_replace('/\x{0140}/u', "l", $string); // ? => l
+		$string = preg_replace('/\x{0142}/u', "l", $string); // l => l
+		$string = preg_replace('/\x{014a}/u', "N", $string); // ? => N
+		$string = preg_replace('/\x{0149}/u', "n", $string); // ? => n
+		$string = preg_replace('/\x{014b}/u', "n", $string); // ? => n
+		$string = preg_replace('/\x{00d8}/u', "O", $string); // Ø => O
+		$string = preg_replace('/\x{00f8}/u', "o", $string); // ø => o
+		$string = preg_replace('/\x{017f}/u', "s", $string); // ? => s
+		$string = preg_replace('/\x{00de}/u', "T", $string); // Þ => T
+		$string = preg_replace('/\x{0166}/u', "T", $string); // T => T
+		$string = preg_replace('/\x{00fe}/u', "t", $string); // þ => t
+		$string = preg_replace('/\x{0167}/u', "t", $string); // t => t
+
+		$string = mb_strtolower($string);
+		return $string;
+	}
 }
 ?>
