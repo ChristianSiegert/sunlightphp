c8876074ec9463d1f404d273a637a992ee6ecb68
Christian Siegert <christian@christian-siegert.com>
2010-08-28 08:54:52 +0200

    Changed debug() function to support an unlimited number of arguments.

--- sunlight/basics.php
+++ sunlight/basics.php
@@ -1,16 +1,18 @@
 <?php
 /**
- * Outputs the formatted value, prefixed with the filename and line number.
+ * Prints the arguments with filename and line number.
  *
  * @param mixed $value
  */
-function debug($value) {
+function debug() {
 	$backtrace = debug_backtrace();
 
-	printf('<pre><span style="display: block; font-weight: bold;">%s (line %s)</span>%s</pre>',
-		substr($backtrace[0]["file"], strlen(ROOT_DIR) + 1),					// Filename
-		$backtrace[0]["line"],													// Line number
-		htmlentities(print_r($value, true), ENT_QUOTES, mb_internal_encoding())	// Text
-	);
+	foreach (func_get_args() as $argument) {
+		printf('<pre><span style="display: block; font-weight: bold;">%s (line %s)</span>%s</pre>',
+			substr($backtrace[0]["file"], strlen(ROOT_DIR) + 1),						// Filename
+			$backtrace[0]["line"],														// Line number
+			htmlentities(print_r($argument, true), ENT_QUOTES, mb_internal_encoding())	// Text
+		);
+	}
 }
 ?>
