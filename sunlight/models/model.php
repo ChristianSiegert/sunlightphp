4bd27361be12050326a4d5ac176f248cf7ebd424
Christian Siegert <christian@christian-siegert.com>
2010-09-03 05:37:04 +0200

    Changed Model::getDocument() to support revision parameter. Added proper document validation mechanism. Reworked core validation rules to be more robust.

--- sunlight/models/model.php
+++ sunlight/models/model.php
@@ -6,7 +6,7 @@ class Model {
 
 	public $modelName;
 
-	public $validate = array();
+	public $validationRules = array();
 	public $validationErrors = array();
 
 	public function __construct(&$controller) {
@@ -58,8 +58,24 @@ class Model {
 		return array($status, $header, $data);
 	}
 
-	public function getDocument($documentId, $method = "GET") {
-		$url = DATABASE_HOST . "/" . rawurlencode(DATABASE_NAME) . "/" . rawurlencode($documentId);
+	/**
+	 * Retrieves a document from CouchDB.
+	 *
+	 * Throws exception on error or if document does not exist. Accepts any
+	 * parameters CouchDB supports.
+	 *
+	 * @param string $documentId The id of the document to retrieve
+	 * @param string $revision The revision of the document to retrieve
+	 * @param array $parameters Parameters used when querying CouchDB
+	 * @param string $method HTTP method used when querying CouchDB
+	 * @return array Array containing HTTP response header and document
+	 */
+	public function getDocument($documentId, $revision = "", $parameters = array(), $method = "GET") {
+		if (!empty($revision)) {
+			$parameters["rev"] = $revision;
+		}
+
+		$url = DATABASE_HOST . "/" . rawurlencode(DATABASE_NAME) . "/" . rawurlencode($documentId) . $this->encodeParameters($parameters);
 		list($status, $header, $data) = $this->query($url, $method);
 
 		if ($status === 200) {
@@ -67,13 +83,20 @@ class Model {
 		} elseif ($status === 404) {
 			throw new Exception("The document you try to fetch does not exist.");
 		} else {
+			debug($header, $data);
 			throw new Exception("The document could not be fetched (Status $status).");
 		}
 	}
 
+	/**
+	 * Checks if document exists.
+	 *
+	 * @param string $documentId
+	 * @return bool
+	 */
 	public function documentExists($documentId) {
 		try {
-			$this->getDocument($documentId, "HEAD");
+			$this->getDocument($documentId, "", array(), "HEAD");
 			return true;
 		} catch (Exception $exception) {
 			return false;
@@ -92,29 +115,29 @@ class Model {
 			}
 		}
 
-		// Add _id field
-		$data["_id"] = $documentId;
-
 		// Add type field
 		if (!isset($data["type"])) {
 			$data["type"] = lcfirst($this->modelName);
 		}
 
-		// Validate data
-		$this->validationErrors = $this->validate($data);
-		$this->controller->validationErrors = $this->validationErrors;
+		// Validate data if rules are defined for this document type
+		if (isset($this->validationRules[$data["type"]])) {
+			$this->validationErrors = $this->validate($data, $this->validationRules[$data["type"]]);
+			$this->controller->validationErrors = $this->validationErrors;
+		} else {
+			throw new Exception("Please define validation rules. Not validating data is a security risk. Aborted storing document.");
+		}
 
 		if (empty($this->validationErrors)) {
-			$url = DATABASE_HOST . "/" . rawurlencode(DATABASE_NAME) . "/" . rawurlencode($data["_id"]);
-
+			$url = DATABASE_HOST . "/" . rawurlencode(DATABASE_NAME) . "/" . rawurlencode($documentId);
 			list($status, $header, $data) = $this->query($url, "PUT", json_encode($data));
 
 			if ($status === 201) {
 				return array($header, $data);
 			} elseif ($status === 409) {
-				throw new Exception("A document with this _id already exists.");
+				throw new Exception("A document with this id already exists.");
 			} else {
-				throw new Exception("Document could not be saved (Status $status).");
+				throw new Exception("Document could not be stored (Status $status).");
 			}
 		} else {
 			throw new Exception("Data is not valid. Aborted storing document.");
@@ -133,8 +156,14 @@ class Model {
 			$revision = $this->getRevision($documentId);
 		}
 
-		$url = DATABASE_HOST . "/" . rawurlencode(DATABASE_NAME) . "/" . rawurlencode($documentId) . "?rev=" . rawurlencode($revision);
-		return $this->query($url, "DELETE");
+		$url = DATABASE_HOST . "/" . rawurlencode(DATABASE_NAME) . "/" . rawurlencode($documentId) . $this->encodeParameters(array("rev" => $revision));
+		list($status, $header, $data) = $this->query($url, "DELETE");
+
+		if ($status === 200) {
+			return array($header, $data);
+		} else {
+			throw new Exception("Document could not be deleted (Status $status).");
+		}
 	}
 
 	/**
@@ -144,7 +173,7 @@ class Model {
 	 * @return string Document revision ("_rev")
 	 */
 	public function getRevision($documentId) {
-		list($header) = $this->getDocument($documentId, "HEAD");
+		list($header) = $this->getDocument($documentId, "", array(), "HEAD");
 
 		if (isset($header["Etag"])) {
 			if (preg_match('/^"([^"]+)"$/', $header["Etag"], $eTag) === 1) {
@@ -158,23 +187,21 @@ class Model {
 	}
 
 	public function getView($designName, $viewName, $parameters = array(), $method = "GET", $data = null, $jsonDecodeResponse = true) {
-		$parametersAsString = "?";
-
-		foreach ($parameters as $parameterName => $parameterValue) {
-			$parametersAsString .= $parameterName . "=" . rawurlencode(json_encode($parameterValue)) . "&";
-		}
-
 		if ($data !== null) {
 			$data = json_encode($data);
 		}
 
-		$url = DATABASE_HOST . "/" . rawurlencode(DATABASE_NAME) . "/_design/" . rawurlencode($designName) . "/_view/" . rawurlencode($viewName) . $parametersAsString;
+		$url = DATABASE_HOST . "/" . rawurlencode(DATABASE_NAME) . "/_design/" . rawurlencode($designName) . "/_view/" . rawurlencode($viewName) . $this->encodeParameters($parameters);
 		list($status, $header, $data) = $this->query($url, $method, $data, $jsonDecodeResponse);
 
 		if ($status === 200) {
 			return array($header, $data);
 		} elseif ($status === 400) {
-			throw new Exception("CouchDB: Bad request: Invalid UTF-8 JSON.");
+			if (isset($data["error"]) && isset($data["reason"])) {
+				throw new Exception("CouchDB: {$data["error"]} &hellip; {$data["reason"]}.");
+			} else {
+				throw new Exception("CouchDB: Something is wrong with the request.<pre>" . print_r($data, true) . "</pre>");
+			}
 		} elseif ($status === 404) {
 			throw new Exception("CouchDB: View '$viewName' does not exist for design '$designName'.");
 		} else {
@@ -182,30 +209,53 @@ class Model {
 		}
 	}
 
-	public function validate($data) {
+	public function encodeParameters($parameters) {
+		$parametersAsString = "?";
+
+		foreach ($parameters as $parameterName => $parameterValue) {
+			if ($parameterName === "rev") {
+				$parametersAsString .= $parameterName . "=" . rawurlencode($parameterValue) . "&";
+			} else {
+				$parametersAsString .= $parameterName . "=" . rawurlencode(json_encode($parameterValue)) . "&";
+			}
+		}
+
+		return $parametersAsString;
+	}
+
+	public function validate($document, $rules) {
 		$validationErrors = array();
 
-		foreach ($this->validate as $fieldName => $ruleSet) {
-			if (!empty($ruleSet)) {
-				foreach ($ruleSet as $ruleName => $rule) {
-					if (!isset($data[$fieldName])) {
-						$data[$fieldName] = "";
-					}
+		foreach ($rules as $fieldName => $rule) {
+			if (!empty($rule)) {
+				if (!isset($document[$fieldName])) {
+					$document[$fieldName] = null;
+				}
 
-					if (is_string($rule["rule"])) {
-						$validates = $this->$rule["rule"]($data[$fieldName]);
-					} elseif (is_array($rule["rule"])) {
-						$validates = $this->$rule["rule"][0]($data[$fieldName], $rule["rule"]);
-					}
+				if (is_string($rule)) {
+					$validates = $this->$rule($document[$fieldName]);
 
 					if (!$validates) {
-						if (isset($rule["message"])) {
-							$errorMessage = $rule["message"];
-						} else {
-							$errorMessage = $ruleName;
-						}
+						$validationErrors[$fieldName] = "Invalid $fieldName.";
+					}
+				} elseif (is_array($rule)) {
+					if (isset($rule[0])) {
+						$function = array($this, $rule[0]);
 
-						$validationErrors[$fieldName][] = $errorMessage;
+						$parameters = $rule;
+						$parameters[0] = $document[$fieldName];
+
+						$validates = call_user_func_array($function, $parameters);
+
+						if (!$validates) {
+							$validationErrors[$fieldName] = "Invalid $fieldName.";
+						}
+					} else {
+						$errors = $this->validate($document[$fieldName], $rules[$fieldName]);
+
+						if (!empty($errors)) {
+							$validationErrors[$fieldName] = $errors;
+						}
 					}
 				}
 			}
@@ -219,23 +269,19 @@ class Model {
 	}
 
 	public function isNumeric($value) {
-		return preg_match('/^[0-9]+$/', $value);
+		return (is_string($value) || is_integer($value)) && preg_match('/^[0-9]+$/', $value);
 	}
 
-	public function isInRange($value, $arguments) {
-		if ($this->isNumeric($value) && $value >= $arguments[1] && $value <= $arguments[2]) {
-			return true;
-		}
-
-		return false;
+	public function isInRange($value, $min, $max) {
+		return $this->isNumeric($value) && $value >= $min && $value <= $max;
 	}
 
 	public function isTimestamp($value) {
-		if ($this->isNumeric($value) && $value >= 0 && $value <= time()) {
-			return true;
-		}
+		return $this->isNumeric($value) && $value >= 0 && $value <= time();
+	}
 
-		return false;
+	public function isUrl($value) {
+		return is_string($value) && preg_match('#^https?://#', $value);
 	}
 }
 ?>
