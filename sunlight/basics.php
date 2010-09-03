06d94eedb5ab65ac1bd31ff81a3875584d6d0937
Christian Siegert <christian@christian-siegert.com>
2010-09-03 05:27:26 +0200

    Added custom formatting function for debug().

--- sunlight/basics.php
+++ sunlight/basics.php
@@ -1,18 +1,56 @@
 <?php
 /**
- * Prints the arguments with filename and line number.
+ * Prints human-readable information about expressions.
  *
- * @param mixed $value
+ * @param mixed $expression,... Unlimited number of expressions
  */
 function debug() {
 	$backtrace = debug_backtrace();
 
 	foreach (func_get_args() as $argument) {
 		printf('<pre><span style="display: block; font-weight: bold;">%s (line %s)</span>%s</pre>',
-			substr($backtrace[0]["file"], strlen(ROOT_DIR) + 1),						// Filename
-			$backtrace[0]["line"],														// Line number
-			htmlentities(print_r($argument, true), ENT_QUOTES, mb_internal_encoding())	// Text
+			substr($backtrace[0]["file"], strlen(ROOT_DIR) + 1),					// Filename
+			$backtrace[0]["line"],													// Line number
+			htmlentities(express($argument), ENT_QUOTES, mb_internal_encoding())	// Text
 		);
+	}
+}
+
+/**
+ * Returns information about an expression. The output is valid PHP code.
+ *
+ * @param string $expression
+ * @param integer $nestingLevel
+ */
+function express($expression, $nestingLevel = 0) {
+	$type = gettype($expression);
+
+	switch ($type) {
+		case "NULL":
+			return "null";
+		case "boolean":
+			return $expression === true ? "true" : "false";
+		case "integer":
+			return $expression;
+		case "double":
+			return $expression;
+		case "string":
+			return "\"$expression\"";
+		case "array":
+			$output = "array(";
+
+			foreach ($expression as $key => $value) {
+				$output .= "\n" . str_repeat("    ", $nestingLevel + 1) . express($key) . " => " . express($value, $nestingLevel + 1) . ",";
+			}
+
+			$output = preg_replace("/,$/", "\n" . str_repeat("    ", $nestingLevel), $output);
+
+			$output .= ")";
+			return $output;
+		case "object":
+			return "(... object ...)";
+		default:
+			return "($type) $expression";
 	}
 }
 ?>
