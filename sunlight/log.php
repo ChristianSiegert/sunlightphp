bb2d5251b7c0b2cc4e38a9972c819e8a58aee0a8
Christian Siegert <christian@christian-siegert.com>
2010-10-05 21:13:37 +0100

    Removed LogComponent. Added Log as static core class.

--- sunlight/log.php
+++ sunlight/log.php
@@ -1,16 +1,17 @@
 <?php
-class LogComponent {
+class Log {
 	/**
-	 * Writes error messages to the error log file.
+	 * Writes a message to the specified log file.
 	 *
-	 * @param string $message Message added to the error log.
-	 * @return boolean Returns false on failure, true on success.
+	 * @param string $message Message to be logged.
+	 * @param string $type Type of log. Default is "errors".
+	 * @return boolean True on success, false on failure.
 	 */
-	public function addError($message) {
+	public static function write($message, $type = "errors") {
 		$backtrace = debug_backtrace();
 
 		$path = APP_DIR . DS . "tmp" . DS . "logs";
-		$filename =  $path . DS . date("Y-m-d") . ".errors";
+		$filename =  $path . DS . date("Y-m-d") . ".$type";
 		$logEntry = date("H:i:s") . " " . $backtrace[1]["class"] . $backtrace[1]["type"] . $backtrace[1]["function"] . "() line " . $backtrace[0]["line"] . ": $message\n";
 
 		return is_writable($path) && ($handle = fopen($filename, "a")) && fwrite($handle, $logEntry) && fclose($handle);
