adad3e000f264fbc89df8c074c6499178caa0df7
Christian Siegert <christian@christian-siegert.com>
2011-02-09 19:58:49 +0100

    Added unfinished e-mail component and unit test.

---
+++ sunlight/controllers/components/email.php
@@ -0,0 +1,45 @@
+<?php
+class EmailComponent {
+	public $from = null;
+
+	public $to = null;
+
+	public $cc = null;
+
+	public $bcc = null;
+
+	public $subject = null;
+
+	public $message = null;
+
+	public function send() {
+		$additionalHeaders = "";
+
+		if (isset($this->from)) {
+			$additionalHeaders .= "From: " . filter_var($this->from, FILTER_SANITIZE_EMAIL) . "\n";
+		}
+
+		if (isset($this->to)) {
+			$validates = filter_var($this->to, FILTER_VALIDATE_EMAIL);
+		}
+
+		if (isset($this->cc)) {
+			$additionalHeaders .= "CC: " . filter_var($this->cc, FILTER_SANITIZE_EMAIL) . "\n";
+		}
+
+		if (isset($this->bcc)) {
+			$additionalHeaders .= "BCC: " . filter_var($this->bcc, FILTER_SANITIZE_EMAIL) . "\n";
+		}
+
+		// Set e-mail encoding to UTF-8
+		mb_language("uni");
+
+		return mb_send_mail(
+			$this->to,
+			$this->subject,
+			$this->message,
+			$additionalHeaders
+		);
+	}
+}
+?>
---
+++ sunlight/tests/cases/controllers/components/email.test.php
@@ -0,0 +1,22 @@
+<?php
+include(CORE_DIR . DS . "controllers" . DS . "components" . DS . "email.php");
+
+class EmailComponentDataTest extends PHPUnit_Framework_TestCase {
+	/**
+	 * @dataProvider isValidAddressDataProvider
+	 */
+	public function testIsValidAddress($address, $expected) {
+		$email = new EmailComponent();
+		$result = $email->isValidAddress($address);
+		$this->assertSame($expected, $result);
+	}
+
+	public function isValidAddressDataProvider() {
+		return array(
+			array("ben@example.com", true),
+			array("Ben <ben@example.com>", true),
+			array("! # $ % & ' * + - / = ? ^ _ ` { | } ~", "Tom &amp; Jerry"),
+		);
+	}
+}
+?>
