624478a002411f42a66b445537aded6eb83ad195
Christian Siegert <christian@christian-siegert.com>
2011-04-03 18:42:50 +0200

    Added String::camelCaseToLowerCaseDash(). Fixed wrong variable name in String::camelCaseToLowerCaseUnderscore().

--- sunlight/libraries/string.php
+++ sunlight/libraries/string.php
@@ -1,7 +1,11 @@
 <?php
 class String {
 	public static function camelCaseToLowerCaseUnderscore($string) {
-		return ltrim(strtolower(preg_replace('#([A-Z])#', "_$1", $className)), "_");
+		return ltrim(strtolower(preg_replace('#([A-Z])#', "_$1", $string)), "_");
+	}
+
+	public static function camelCaseToLowerCaseDash($string) {
+		return ltrim(strtolower(preg_replace('#([A-Z])#', "-$1", $string)), "-");
 	}
 }
 ?>
